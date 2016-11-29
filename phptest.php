8bc0bc2b380520b07be2a7f9867658b398b4f9ff
Phoenixcon <krislegg2@gmail.com>
2016-11-28 22:24:02 -0600

    Minor formatting changes

--- phptest.php
+++ phptest.php
@@ -18,7 +18,6 @@ $db = mysqli_connect($host,$username,$password,'recipes')
         <link rel="stylesheet" type="text/css" href="css/index.css">
     </head>
     <body>
-        <h1>PHP Connection Success</h1>
         <?php
         $name_query = "SELECT * FROM recipe_name";
         $credit_query = "SELECT * FROM recipe_credit";
@@ -56,24 +55,21 @@ $db = mysqli_connect($host,$username,$password,'recipes')
         while ($row = mysqli_fetch_assoc($result)) {
             $recipe_tags[] = $row;
         }
-
-        //echo "[{";
-        //foreach ($recipe_names as $key => $row) {
-        //    echo '"'.'Recipe Name'.'":"'.$row['Recipe Name'].'"';
-        //}
-        //$recipe_name = array();
-
-        $recipe_name_select = "SELECT * FROM recipe_name";
+        
+        //SELECT RECIPE NAMES
+        $recipe_name_select = "SELECT * FROM recipe_name"; 
         $name_result = mysqli_query($db, $recipe_name_select);
         while ($row = mysqli_fetch_assoc($name_result)) {
             echo $row['recipename'].'<br>';
             $recipe_name=$row['recipename'];
 
+            //SELECT RECIPE-CREDIT KEYS
             $recipe_credit_select = "SELECT creditkey FROM recipe_name WHERE recipename='".$recipe_name."'";
             $credit_result = mysqli_query($db, $recipe_credit_select);
             while ($credit_row = mysqli_fetch_assoc($credit_result)) {
                 $creditkey=$credit_row['creditkey'];
 
+                //SELECT RECIPE-CREDIT TEXT
                 $credit_text_select = "SELECT credittext FROM recipe_credit WHERE creditkey='".$creditkey."'";
                 $credit_text_result = mysqli_query($db, $credit_text_select);
                 while ($credit_text_row = mysqli_fetch_assoc($credit_text_result)) {
@@ -81,36 +77,43 @@ $db = mysqli_connect($host,$username,$password,'recipes')
                 }
             }
 
+            //SELECT MAIN RECIPE-KEYS
             $recipe_key_select = "SELECT recipekey FROM recipe_name WHERE recipename='".$recipe_name."'";
             $key_result = mysqli_query($db, $recipe_key_select);
             while ($key_row = mysqli_fetch_assoc($key_result)) {
                 $recipekey=$key_row['recipekey'];
 
+                //SELECT RECIPE INGREDIENTS
                 $recipe_ingredient_select = "SELECT ingredientname FROM recipe_ingredients WHERE recipekey='".$recipekey."'";
                 $ingredient_result = mysqli_query($db, $recipe_ingredient_select);
                 while ($ingredient_row = mysqli_fetch_assoc($ingredient_result)) {
                     echo $ingredient_row['ingredientname'].'<br>';
                 }
 
+                //SELECT RECIPE INSTRUCTIONS
                 $recipe_instruction_select = "SELECT instructionstep,textdescription FROM recipe_instructions WHERE recipekey='".$recipekey."'";
                 $instruction_result = mysqli_query($db, $recipe_instruction_select);
                 while ($instruction_row = mysqli_fetch_assoc($instruction_result)) {
                     echo $instruction_row['instructionstep'].'. '.$instruction_row['textdescription'].'<br>';
                 }
 
+                //SELECT RECIPE-TAG KEYS
                 $recipe_tagkey_select = "SELECT tagkey FROM recipe_name_tag WHERE recipekey='".$recipekey."'";
                 $tagkey_result = mysqli_query($db, $recipe_tagkey_select);
                 while ($tagkey_row = mysqli_fetch_assoc($tagkey_result)) {
                     $tagkey = $tagkey_row['tagkey'];
 
+                    //SELECT RECIPE-TAG TEXT
                     $recipe_tag_select = "SELECT tagtext FROM recipe_tags WHERE tagkey='".$tagkey."'";
                     $tag_result = mysqli_query($db, $recipe_tag_select);
                     while ($tag_row = mysqli_fetch_assoc($tag_result)) {
-                        echo $tag_row['tagtext'].'<br>';
+                        echo 'Tags: '.$tag_row['tagtext'].' ';
                     }
                 }
             }
+            echo '<br>';
         }
+        
 
 
         //echo json_encode($recipe_names).'<br /><br />';
