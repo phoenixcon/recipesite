fb566ef3bef41a3916f6cfe591e3b23248321f6c
Phoenixcon <krislegg2@gmail.com>
2016-11-30 14:08:05 -0600

    Working on form submission to database

--- scripts/handle.php
+++ scripts/handle.php
@@ -28,16 +28,64 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $errors['tag']='At least one Tag is required.';
 
 
-    //include('connection-local.php'); //mySQL Connection
+    include('connection-local.php'); //mySQL Connection
+    
+    $credit_check_query = "SELECT creditkey FROM recipe_credit WHERE credittext = '".$form_credit."'";
+    $update_credit_table = "INSERT INTO recipe_credit(creditkey, credittext) VALUES(null, '$form_credit')";
+    
+    $credit_check_result = mysqli_query($db, $credit_check_query);
+    
+    if (mysqli_num_rows ($credit_check_result)>0) {
+        $credit_check_row = mysqli_fetch_assoc($credit_check_result);
+        $credit_check_key = $credit_check_row['creditkey'];
+    } else {
+        mysqli_query($db, $update_credit_table);
+        $credit_check_result = mysqli_query($db, $credit_check_query);
+        $credit_check_row = mysqli_fetch_assoc($credit_check_result);
+        $credit_check_key = $credit_check_row['creditkey'];
+    }
+    
+    $update_name_table = "INSERT INTO recipe_name(recipekey, recipename, creditkey) VALUES(null, '$form_name', '".$credit_check_key."')";
+    $recipe_key_query = "SELECT recipekey FROM recipe_name WHERE recipename ='".$form_name."'";
+    $recipe_key_result = mysqli_query($db, $recipe_key_query);
+    $recipe_key_row = mysqli_fetch_assoc($recipe_key_result);
+    $recipe_key = $recipe_key_row['recipekey'];
+    
+    $ingredients = array();
+    foreach( $form_ingredient as $ingredientrow ){
+        $ingredients[] = '(null, '.$recipe_key.', "'.$ingredientrow.'")';
+    }
+    $update_ingredient_table = "INSERT INTO recipe_ingredients(ingredientkey, recipekey, ingredientname) VALUES".implode(',', $ingredients)."";
+    echo $update_ingredient_table;
+    
+    
+    $instructions = array();
+    foreach( $form_instruction as $instructionrow ){
+        $instructions[] = '(null, '.$recipe_key.', "'.$instructionrow.'", null)';
+    }
+    $update_instruction_table = "INSERT INTO recipe_instructions(instructionkey, recipekey, textdescription, instructionstep) VALUES".implode(',', $instructions)."";
+    echo $update_instruction_table;
+    
+    
+    $tags = array();
+    foreach( $form_tag as $tagrow ){
+        $tags[] = '(null, "'.$tagrow.'")';
+    }
+    $update_tag_table = "INSERT INTO recipe_tags(tagkey, tagtext) VALUES".implode(',', $tags)."";
+    echo $update_tag_table;
+    
+    
+    mysqli_query($db, $update_name_table);
+    mysqli_query($db, $update_ingredient_table);
+    mysqli_query($db, $update_instruction_table);
+    mysqli_query($db, $update_tag_table);
+    
 
-    //if ($form_name == 'Kris') {
-    //    $kris_date_check_result = mysqli_query($dbc, "SELECT * FROM kris_workouts WHERE date = '$form_date'");
-    //    if (mysqli_num_rows ($kris_date_check_result)>0) {
-    //        $errors['date_duplicate']='Entry for '.$form_date.' already exists.';
-    //    }
-    //}
+    $update_instruction_table = '';
+    $update_tag_table = '';
+    $update_name_tag_table = '';
 
-    //mysqli_close ($dbc);
+    mysqli_close ($db);
 
     if (!empty($errors)) {
         $data['success'] = false;
@@ -75,10 +123,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 //}
 
 //echo json_encode($data);
-var_dump($form_name);
-var_dump($form_credit);
-var_dump($form_ingredient);
-var_dump($form_instruction);
-var_dump($form_tag);
+//var_dump($form_name);
+//var_dump($form_credit);
+//var_dump($form_ingredient);
+//var_dump($form_instruction);
+//var_dump($form_tag);
+//var_dump($credit_check_query);
 
 ?>
