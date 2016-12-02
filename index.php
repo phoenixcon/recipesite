66b11285a05524482508d84e791c40e63fd54c96
Phoenixcon <krislegg2@gmail.com>
2016-12-02 13:45:02 -0600

    Updated mobile styling

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <title>Legg Family Recipes</title>
         <meta charset="utf-8"  />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="scripts/masonry.pkgd.min.js"></script>
